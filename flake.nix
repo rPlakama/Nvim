70d893c377f3d1b8ac4028b72f5eb561afc18c15
rPlakama <rPlakama@proton.me>
2025-11-12 20:42:03 -0300

    Flake for keeping things clean.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Configurações NVIM";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+      lua-language-server
+      ];
+
+      shellHook = ''
+        echo "JS ENV Loaded."
+      '';
+    };
+  };
+}
