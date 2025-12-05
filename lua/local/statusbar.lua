36f3d5270dd52d38c3bcc8b49716f9a63484f615
rPlakama <rPlakama@proton.me>
2025-12-05 10:54:37 -0300

    statusline (self-made, local)

---
+++ lua/local/statusbar.lua
@@ -0,0 +1,36 @@
+local function get_git_branch()
+	local handle = io.popen("git branch --show-current 2> /dev/null")
+	if handle then
+		local branch = handle:read("*a")
+		handle:close()
+		if branch and branch ~= "" then
+			return string.gsub(branch, "\n", "")
+		end
+	end
+	return ""
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
+	callback = function()
+		vim.b.git_branch = get_git_branch()
+	end,
+})
+
+function MyStatusLine()
+	local branch = vim.b.git_branch or ""
+	if branch ~= "" then
+		branch = "  " .. branch .. " | "
+	end
+
+	return table.concat({
+		"%#Normal#",
+		branch,
+		" %f",
+		"%m",
+		"%=",
+		"%y ",
+		" %p%% ",
+	})
+end
+
+vim.opt.statusline = "%!v:lua.MyStatusLine()"
