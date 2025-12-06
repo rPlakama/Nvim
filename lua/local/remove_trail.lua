759007e9c02592d599378d9139991aa670ec79c7
rPlakama <rPlakama@proton.me>
2025-12-06 13:16:27 -0300

    Remove trailling on save.

---
+++ lua/local/remove_trail.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd('BufWritePre', {
+	desc = 'Removes trailing whitespace on save',
+	callback = function()
+		local save_cursor = vim.fn.getpos('.')
+		vim.cmd([[%s/\s\+$//e]])
+		vim.fn.setpos('.', save_cursor)
+	end,
+})
