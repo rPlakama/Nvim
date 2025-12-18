594dcd267f385f46fe1b3b6abfeb11c3198216b8
rPlakama <rPlakama@proton.me>
2025-12-18 17:46:06 -0300

    More elegant I guess

--- lua/config/opts/etc.lua
+++ lua/config/opts/etc.lua
@@ -8,7 +8,23 @@ vim.opt.autoread = true
 vim.api.nvim_set_hl(0, 'LspReferenceText', { ctermbg = 237 })
 vim.api.nvim_set_hl(0, 'LspReferenceRead', { ctermbg = 237 })
 vim.api.nvim_set_hl(0, 'LspReferenceWrite', { ctermbg = 237 })
+vim.api.nvim_set_hl(0, "Pmenu", { link = "Normal" })
 
 vim.api.nvim_set_hl(0, 'StatusLine', {
 	reverse = false
+})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		local highlight_groups = {
+			"FloatBorder", "Normal", "NormalNC", "NormalFloat", "FloatBorder",
+			"Pmenu", "SignColumn", "LineNr", "EndOfBuffer"
+		}
+		for _, group in ipairs(highlight_groups) do
+			vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+		end
+
+		vim.api.nvim_set_hl(0, "PmenuSel", { bg = "NONE", reverse = true })
+	end,
 })
